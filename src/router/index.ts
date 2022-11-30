1fa21aae2cb48af0c5b9d24934a401f95c778910
lzp885387491 <885387491@qq.com>
2022-11-30 17:30:00 +0800

    add login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Layout from '../views/layout/layout.vue'
+import Login from '../views/Login/login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,16 +9,20 @@ const router = createRouter({
       path: '/',
       name: 'layout',
       component: Layout,
-      children:[
-      {
-        path:'/userAdmin',
-        name:'userAdmin',
-        component:()=> import('@/views/user/UserAdmin.vue')
-      },{
-        path:'/corporateManagement',
-        name:'corporateManagement',
-        component:()=> import('@/views/corporateManagement/CorporateManagement.vue')
-      }
+      children: [
+        {
+          path: '/userAdmin',
+          name: 'userAdmin',
+          component: () => import('@/views/user/UserAdmin.vue')
+        }, {
+          path: '/corporateManagement',
+          name: 'corporateManagement',
+          component: () => import('@/views/corporateManagement/CorporateManagement.vue')
+        }, {
+          path: '/login',
+          name: 'Login',
+          component: Login,
+        }
       ]
     },
   ]
