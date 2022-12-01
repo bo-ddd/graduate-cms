009afa27e4d15f02b688013949a846b68746294d
lzp885387491 <885387491@qq.com>
2022-12-01 15:36:50 +0800

    Update login add main.ts code

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -5,13 +5,13 @@
                 <div class="form_title">登录</div>
                 <div class="form_input">
                     <el-form-item label="USERNAME">
-                        <el-input type="text" v-model="form.username" />
+                        <el-input v-model="form.username" placeholder="请输入用户名" maxlength="15" type="text" />
                     </el-form-item>
                     <el-form-item label="PASSWORD">
-                        <el-input type="password" v-model="form.password" />
+                        <el-input v-model="form.password" placeholder="请输入密码" maxlength="15" type="password" />
                     </el-form-item>
                 </div>
-    
+
                 <div class="form_submit">
                     <el-button type="primary" @click="submit">登录</el-button>
                     <el-button>注册</el-button>
@@ -23,6 +23,8 @@
 
 <script setup lang="ts">
 import { reactive } from 'vue';
+import { ElMessage } from 'element-plus'
+
 interface Form {
     username: string,
     password: string,
@@ -31,31 +33,85 @@ const form: Form = reactive({
     username: '',
     password: '',
 });
+interface MessageProps {
+    message: string,
+    type: "warning" | "success" | "info" | "error"
+}
+const formCheck = function (form: Form) {
+    let res: MessageProps = {
+        message: '',
+        type: 'warning'
+    }
+    for (const key in form) {
+        if (form[key as keyof typeof form].length < 6) {
+            res.message = `${key}的长度不能小于6位数!`;
+            return res;
+        } else if (form[key as keyof typeof form] == '' || form[key as keyof typeof form] == undefined) {
+            res.message = `${key}不能为空!`;
+            return res;
+        } else {
+            res.type = 'success';
+            res.message = '登录成功';
+        }
+    }
+    return res;
+}
+
 const submit = function () {
+    let res = formCheck(form);
+    ElMessage({
+        type: res.type,
+        message: res.message
+    })
     console.log(form);
 };
 </script>
 
 <style scoped lang="scss">
 :deep(.el-form) {
-    padding: 18px 30PX;
+    padding: 30px 50PX;
     border: 1px solid var(--el-border-color);
     color: #409EFF;
-    background-color: rgba(255, 255, 255, 0.4);
+    background-color: rgba(255, 255, 255, .6);
+    border-radius: 5px;
+    box-shadow: 0px 0px 100px 0PX rgb(157, 155, 155);
 }
-:deep(.el-form-item__label){
+
+:deep(.el-form-item__label) {
     color: #409EFF;
 }
-.login{
+
+.login {
     width: 100vw;
     height: 100vh;
     background-image: url('@/assets/imanges/5.png');
     background-repeat: no-repeat;
-    background-size:cover;
+    background-size: cover;
 }
-:deep(.el-input){
-    background-color: red;
+
+:deep(.el-input__inner) {
+    color: #409EFF !important;
+    outline: none !important;
+    border: none;
 }
+
+:deep(.el-input__wrapper) {
+    box-shadow: 0 0 0 0px var(--el-input-border-color, var(--el-border-color)) inset;
+    cursor: default;
+    border-radius: 0;
+    background-color: rgba(0, 0, 0, 0);
+    padding: 0 10px;
+    border-bottom: 1px solid #409EFF;
+
+    .el-input__inner {
+        cursor: default !important;
+    }
+}
+
+:deep(.el-form-item label) {
+    font-size: 14px;
+}
+
 .form {
     width: 100vw;
     height: 100vh;
@@ -63,9 +119,11 @@ const submit = function () {
     align-items: center;
     justify-content: center;
     flex-direction: column;
+
     .form_title {
         text-align: center;
-        margin-bottom: 18px;
+        font-size: 20px;
+        margin-bottom: 25px;
     }
 
     .form_submit {
@@ -73,7 +131,8 @@ const submit = function () {
         display: flex;
         align-items: center;
         justify-content: center;
-        .el-button{
+
+        .el-button {
             width: 35%;
         }
     }
