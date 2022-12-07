998b2d6938ff1878c78465c6db5f3b8edb795dc2
yang <1145938287@qq.com>
2022-12-07 16:46:36 +0800

    member

--- src/stores/memberCard.ts
+++ src/stores/memberCard.ts
@@ -2,15 +2,9 @@
 import { defineStore } from 'pinia'
 import Api from '../assets/api/member-api'
 
-interface Res{
-code:number,
-msg:string,
-data:object[]
-}
-
 export const useMemberCard = defineStore('member', () => {
     function getMemberCardList(params: any) {
-        return Api.getMemberCardList(params) as any
+        return Api.getMemberCardList(params)
     }
 
     return { getMemberCardList }
