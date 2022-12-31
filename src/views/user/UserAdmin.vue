5617a834a17ad41af382ccd1bd45a39965f3e836
shangyuhao <834725611@qq.com>
2022-12-31 20:13:59 +0800

    useradmin

--- src/views/user/UserAdmin.vue
+++ src/views/user/UserAdmin.vue
@@ -28,8 +28,15 @@ getUserList();
  * 
  */
 let interviewDialog = ref(false);
-const interviewFn = () => {
+let interviewList = ref();
+const interviewFn = async (userId:number) => {
     interviewDialog.value = true;
+    let res = await userStore.selectUserInterview({
+        userId
+    })
+   if(res.code){
+    interviewList.value = res.data;
+   }
 }
 
 /***
@@ -38,15 +45,30 @@ const interviewFn = () => {
  * 
  */
 
+let deliveryId = ref(0);
 let deliveryDialog = ref(false);
+let  deliveryUserId = ref();
+let deliveryList = ref();
 const deliveryFn = async (id?: number) => {
     deliveryDialog.value = true;
-    console.log(id);
+    deliveryUserId.value = id;
     let res = await userStore.selectUserDelivery({
-        deliveryStatus: <number>id || 0
+        deliveryStatus:deliveryId.value,
+        userId:Number(deliveryUserId.value)   
     });
     if (res.code == 200) {
-        console.log(res);
+        deliveryList.value = res.data;
+    }
+}
+
+const deliveryTabFn = async(name:number)=>{
+    deliveryId.value = name;
+    let res = await userStore.selectUserDelivery({
+        deliveryStatus:deliveryId.value,
+        userId:Number(deliveryUserId.value)
+    });
+    if (res.code == 200) {
+        deliveryList.value = res.data;
     }
 }
 
@@ -69,7 +91,7 @@ getStageL();
 let collectionName = ref("企业");
 let collectionDialog = ref(false);
 let bool = ref(true);
-let cardList: any = ref([]);
+let cardList = ref();
 let collUserId = ref();
 const collectionFn = (userId: number) => {
     collectionDialog.value = true;
@@ -86,9 +108,9 @@ const userStarFn = async (name?: string) => {
         bool: bool.value,
         userId: Number(collUserId.value)
     })
-    console.log('-----------', res)
     if (res.code == 200) {
-        cardList.value = res.data;
+        cardList.value = res.data.data;
+
     }
 }
 
@@ -180,12 +202,12 @@ const selectUserOpinion = async (userId: number) => {
                 <el-table-column prop="userStatusName" label="求职状态" />
                 <el-table-column label="投递记录">
                     <template #default="scope">
-                        <div class="red-color" @click="deliveryFn()">详情</div>
+                        <div class="red-color" @click="deliveryFn(scope.row.userId)">详情</div>
                     </template>
                 </el-table-column>
                 <el-table-column label="面试">
                     <template #default="scope">
-                        <div class="red-color" @click="interviewFn()">详情</div>
+                        <div class="red-color" @click="interviewFn(scope.row.userId)">详情</div>
                     </template>
                 </el-table-column>
                 <el-table-column prop="userSite" label="站点" />
@@ -200,59 +222,62 @@ const selectUserOpinion = async (userId: number) => {
 
         <!-- 投递记录 -->
         <el-dialog v-model="deliveryDialog" width="50%" align-center>
-            <el-tabs tab-position="top" :model-value="0" @tab-change="deliveryFn" style="min-height:400px"
+            <el-tabs tab-position="top" :model-value="0" @tab-change="deliveryTabFn" style="height:500px;overflow:auto;"
                 class="delivery">
-                <!-- <el-tab-pane label="待查看">
-                    <div class="cord-box">
-                        <div class="header">
-                            <div class="">
-                                <h4>Java开发工程师</h4>
-                            </div>
-                            <div class="red-ft">8-15k</div>
+                <el-tab-pane v-for="item in stagel" :label="item.label" :name="item.value">
+                    <div class="cord-box" v-for="item in deliveryList">
+                  <div class="header">
+                     <div class=""><h3>{{ item.positionName  }}</h3></div>
+                     <div class="red-ft">{{ item.positionMoney  }}</div>
+                  </div>
+                  <div class="count">
+                     <div class="count-gs">
+                        <div><img :src="item.companyLogoUrl " alt=""></div>
+                        <div class="text-left"> 
+                           {{ item.companyName }}
+                         <br>{{ item.companyAddr }}
                         </div>
-                        <div class="count">
-                            <div class="count-gs">
-                                <div><img src="@/assets/images/icon-file.png" alt=""></div>
-                                <div>
-                                    中科百谷
-                                    <br>北京-北京市
-                                </div>
-                            </div>
-                            <div class="d0-ft">17:41</div>
-                        </div>
-                    </div>
-                    <div class="cord-box">
-                        <div class="header">
-                            <div class="">
-                                <h4>Java开发工程师</h4>
-                            </div>
-                            <div class="red-ft">8-15k</div>
-                        </div>
-                        <div class="count">
-                            <div class="count-gs">
-                                <div><img src="@/assets/images/icon-file.png" alt=""></div>
-                                <div>
-                                    中科百谷
-                                    <br>北京-北京市
-                                </div>
-                            </div>
-                            <div class="d0-ft">17:41</div>
-                        </div>
-                    </div>
-                </el-tab-pane> -->
-                <el-tab-pane v-for="item in stagel" :label="item.label" :name="item.value"></el-tab-pane>
+                       </div>
+                     <div class="d0-ft">{{ item.interviewTime  }}</div>
+                  </div>
+              </div>
+                </el-tab-pane>
             </el-tabs>
         </el-dialog>
 
         <!-- 面试 -->
-        <el-dialog v-model="interviewDialog" width="50%" align-center>
-            interviewDialog
+        <el-dialog v-model="interviewDialog" width="50%" align-center  style="height: 500px;overflow: auto;"    >
+            <div class="inter">
+        <div v-for="item in interviewList" :key="item.companyId">
+            <div>
+                <div class="date_time flex">
+                    {{ item.interviewTime }}
+                </div>
+
+                <div class="just-between">
+                    <div class="title">
+                        <h2>{{ item.companyName }}</h2>
+                        <span>线下面试</span>
+                    </div>
+
+                    <div class="icon-head_portrait">
+                        <img :src="item.companyLogoUrl" alt="">
+                    </div>
+                </div>
+
+                <div class="brief_introduction align-center">
+                    <div>{{ item.positionName }}</div>
+                    <div class="ml-5">{{ item.positionMoney }}</div>
+                </div>
+            </div>
+        </div>
+    </div>
         </el-dialog>
 
 
         <!-- 反馈 -->
         <el-dialog v-model="centerDialogVisible" width="50%" align-center>
-            <el-table :data="opinionData" style="width: 100%;">
+            <el-table :data="opinionData" style="height: 500px;overflow: auto;"  >
                 <el-table-column prop="userName" label="姓名" />
                 <el-table-column prop="userOpinion" label="反馈内容" />
                 <el-table-column prop="userPhone" label="联系方式" />
@@ -261,45 +286,37 @@ const selectUserOpinion = async (userId: number) => {
 
         <!-- 收藏 -->
         <el-dialog v-model="collectionDialog" width="50%" align-center>
-            <el-tabs @tab-change="userStarFn" :model-value="collectionName" tab-position="top" style="height: 200px"
+            <el-tabs @tab-change="userStarFn" :model-value="collectionName" tab-position="top" style="height: 500px;overflow: auto;"
                 class="demo-tabs">
-                <el-tab-pane label="企业" name="企业">企业</el-tab-pane>
+                <el-tab-pane label="企业" name="企业">
+                    <el-empty description="description" />
+                </el-tab-pane>
                 <el-tab-pane label="职位" name="职位">
-                    <div class="wrap item just-center" v-for="item, index in cardList.data" :key="index">
-                        <div class="container">
-                            <div class="header">
-                                <p class="fs-16">{{ item.positionName }}</p>
-                                <div class="align-center  mt-14">
-                                    <span class="fs-14 c-747474">{{item.positionAddr? item.positionAddr.split(',').splice(1,).join('-'):''
-}}</span>
-                                    <span class="fs-12 c-747474 gang">|</span>
-                                    <span class="fs-14 c-747474">{{ item.positionIndustry }}</span>
-                                </div>
-                                <p class="fs-12  c-747474 education flex-ja-center mt-5">{{
-        item.positionEducation
-}}</p>
+                         <div class="positionItem mt-10" v-for="item in cardList">
+                            <div class="top flex-between color-black">
+                                <div>{{ item.positionName }}</div>
+                                <div class="red-color">{{ item.positionMoney }}</div>
                             </div>
-                            <div class="footer mt-14 align-center ">
-                                <img class="icon" :src="item.companyLogoUrl" alt="">
-                                <div class="">
-                                    <p class="fs-14">{{ item.companyName }}</p>
-                                    <span class="fs-12 c-747474">{{ item.companySize }}人</span>
-                                    <span class=" gang c-747474">|</span>
-                                    <span class="fs-12 c-747474">{{
-        item.companyIndustry == 'null' ? '不限' : item.companyIndustry
-}}</span>
+                            <div class="positionItem-header color-black flex">
+                                <div class="">{{item.positionAddr}}&nbsp;|&nbsp;{{ item.positionStatus }}</div>
+                  
+                            </div>
+                            <div class="positionItem-header">
+                                <span class="ss">
+                                    {{ item.positionEducation }}
+                                </span>
+                            </div>
+                            <div class="bottom flex">
+                                <div>
+                                    <img :src="item.companyLogoUrl" alt="">
+                                </div>
+                                <div>
+                                    <div class="text-left">
+                                        {{ item.companyName }}
+                                      <br>{{item.companySize}}&nbsp;&nbsp;|&nbsp;&nbsp;{{ item.companyIndustry }}</div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="operation" @click.stop="">
-                            <p class=" mt-14 c-fb5530 fs-14 fw-600 money">{{ !item.positionMoney ? '' :
-        item.positionMoney.replace(/,/g, '-').replace(/0/g, '') + 'k'
-}}</p>
-                            <!-- <p v-if="item.isDelivery" class="mt-30 fs-12 c-a8a8a8">已申请</p>
-                            <van-button v-if="!item.isDelivery" class="mt-20 btn fw-600" size="mini"
-                                type="primary">申请</van-button> -->
-                        </div>
-                    </div>
+                         </div>
                 </el-tab-pane>
             </el-tabs>
         </el-dialog>
@@ -330,11 +347,137 @@ const selectUserOpinion = async (userId: number) => {
 :deep(.el-dialog) {
     border-radius: 6px;
     text-align: center;
+    background-color: rgb(243, 243, 243);
+}
+
+.inter {
+    padding: 0 20px;
+    width:80%;
+    background: #ffffff;
+    margin: 0 auto;
+    border-radius: 5px;
+    .date_time {
+        padding: 5px 0;
+        color: #0b0a0a;
+    }
+
+    .title {
+        &>h2 {
+            padding: 5px 0;
+            font-size: 1.7rem;
+        }
+
+        &>span {
+            display: inline-block;
+            padding: 5px 0;
+            font-size: 12px;
+            color: #606060;
+        }
+    }
+
+    .icon-head_portrait {
+        border: 10px solid #ccc;
+        border-radius: 30px;
+    }
+
+    .icon-head_portrait,
+    img {
+        width: 45px;
+        height: 45px;
+    }
+
+    .brief_introduction {
+        padding: 10px 0;
+        font-size: 12px;
+        border-bottom: 1px solid #f3f3f3;
+        color: #646464;
+    }
+}
+
+.cord-box{
+     border-bottom: 1px solid rgb(247, 247, 247);
+     border-top: 5px solid rgb(247, 247, 247);
+     .header{
+      display: flex;
+      justify-content: space-between;
+      padding: 10px;
+       .red-ft{
+          color: red;
+          font-weight: 600;
+       }
+     }
+     .count{
+      display: flex;
+      justify-content: space-between;
+      padding:0 10px;
+      .count-gs{
+         display: flex;
+         align-items: center;
+         gap: 0 5px;
+         img{
+            width: 40px;
+            height:40px;
+            align-items: center;
+            gap: 0 10px;
+         }
+      }
+      .d0-ft{
+         color: #a5a5a5;
+         font-size: 10px;
+       }
+     }
+  }
+
+.color-black{
+    color: black;
+}
+.positionItem{
+    padding: 15px;
+    width: 460px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px 0;
+    margin:  0 auto;
+    background-color: #ffffff;
+    border-radius: 5px;
+}
+
+.positionItem-header{
+    display: flex;
+}
+
+.text-left{
+   text-align: left;
+}
+.ss{
+    padding: 2px;
+    background-color: #efefef;
+}
+
+.flex-between{
+    display: flex;
+    justify-content: space-between;
+}
+
+.flex{
+    display: flex;
+   align-items: center;
+   gap: 0 10px;
+}
+
+.mt-10{
+    margin-top: 10px;
+}
+
+.bottom{
+    img{
+        width: 30px;
+        height: 30px;
+    }
 }
 
 .red-color {
     color: red;
-    text-align: center;
 }
 
 :deep(.el-table .cell) {
@@ -357,68 +500,6 @@ const selectUserOpinion = async (userId: number) => {
             justify-content: center;
         }
 
-    }
-
-    .item {
-        justify-content: space-between;
-        background-color: #ffffff;
-
-        .gang {
-            margin: 0 .3rem;
-        }
-
-        .container {
-            .header {
-                padding-top: 1.5rem;
-
-                .education {
-                    width: 3.2rem;
-                    height: 2rem;
-                    background-color: #f2f3f5;
-                }
-            }
-
-            .footer {
-                padding-bottom: 1.5rem;
-                gap: 1rem;
-
-                .icon {
-                    width: 3.2rem;
-                    height: 3.2rem;
-                }
-            }
-        }
-
-        .operation {
-            .content {
-                padding: 2rem;
-
-                .flex {
-                    height: 61vh;
-                    gap: 1rem;
-
-                    .title {
-                        line-height: 2.5rem;
-                    }
-                }
-
-                .btn-confirm {
-                    width: 100%;
-                }
-
-                .to-resume {
-                    min-height: 55vh;
-                }
-            }
-
-            .btn {
-                margin-top: 3rem;
-                border-radius: .5rem;
-                padding: 1.4rem 1rem;
-                background-color: #3b7dff;
-
-            }
-        }
     }
 
     .delivery {
